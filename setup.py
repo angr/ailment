b0d3e01e7b4390978f19b11469ab1f5b831c7d6b
Fish <fish@cs.ucsb.edu>
2017-07-02 00:04:22 -0700

    Some skeleton code.

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+    packages = find_packages()
+except ImportError:
+    from distutils.core import setup
+    import os
+    packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
+
+setup(
+    name='ailment',
+    version='6.7.6.9',
+    packages=packages,
+    install_requires=[],
+    description='The angr intermediate language.',
+    url='https://github.com/angr/ailment',
+)
