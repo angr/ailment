e640ba2f8d69a1ca03e7572e91681d308cc16479
Audrey Dutcher <audrey@rhelmot.io>
2019-06-22 14:21:35 -0700

    Shuffle imports. see angr/angr-doc#278
    Makes it possible to import ailment before angr.

--- ailment/__init__.py
+++ ailment/__init__.py
@@ -1,10 +1,10 @@
 __version__ = (8, 19, 4, 5)
 
 from .block import Block
-from .converter import IRSBConverter
-from .manager import Manager
-
 from . import statement as Stmt
 from . import expression as Expr
 from .statement import Assignment
 from .expression import Expression, Const, Tmp, Register, UnaryOp, BinaryOp
+
+from .converter import IRSBConverter
+from .manager import Manager
