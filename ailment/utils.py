7d32e794dbe59288795433841675d034047e3bfc
Audrey Dutcher <audrey@rhelmot.io>
2021-05-18 23:50:29 -0700

    Handle MultiValues as valid expression operands

--- ailment/utils.py
+++ ailment/utils.py
@@ -1,6 +1,7 @@
 from typing import Union, Optional, TYPE_CHECKING
 
 import claripy
+from angr.storage.memory_mixins.paged_memory.pages.multi_values import MultiValues
 
 if TYPE_CHECKING:
     from .expression import Expression
@@ -12,6 +13,8 @@ def get_bits(expr: Union[claripy.ast.Bits,'Expression',int]) -> Optional[int]:
 
     if isinstance(expr, Expression):
         return expr.bits
+    if isinstance(expr, MultiValues):
+        return get_bits(expr.one_value())
     elif isinstance(expr, claripy.ast.Bits):
         return expr.size()
     elif hasattr(expr, 'bits'):
