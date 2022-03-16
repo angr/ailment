40fc6ad83a37cd18817afdf069861efe34375c30
Kevin Phoenix <kevin@kphoenix.us>
2022-03-15 19:21:01 -0700

    Add pyproject.toml (#83)

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
-
-try:
-    from setuptools import setup
-    from setuptools import find_packages
-    packages = find_packages()
-except ImportError:
-    from distutils.core import setup
-    import os
-    packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
+from setuptools import setup, find_packages
 
 setup(
     name='ailment',
     version='9.1.gitrolling',
     python_requires='>=3.6',
-    packages=packages,
+    packages=find_packages(),
     install_requires=[],
     description='The angr intermediate language.',
     url='https://github.com/angr/ailment',
